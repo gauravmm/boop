81767b3ba8dc63434b42e82f4cbfe131e59f9899
Gaurav Manek <gaurav@gauravmanek.com>
2017-06-24 10:41:58 +0800

    Button and registration semantics.

--- web/main.js
+++ web/main.js
@@ -3,6 +3,8 @@
 // This is based loosely on the tutorial at:
 // https://developers.google.com/web/fundamentals/getting-started/codelabs/push-notifications/
 
+var PUBLIC_KEY="BKgErtbhIt2ODORN94yWQsX-etN4RwaJc3Z7NTD_litd4Zmu3oIN39tjXHd8bEMt5Fl94r9I6YakfEHtWijS96Y";
+
 function $(id) {
     return document.getElementById(id);
 }
@@ -10,22 +12,49 @@ function $(id) {
 function handleSetupError(error){
     console.error('Service Worker Error', error);
     $("notif-enable").className = "button";
+    $("notif-enable").disabled = true;
     $("notif-enable-err").className = "error";
     $("notif-enable-err-msg").innerHTML = error;
 }
 
 var swRegistration = null;
-$("notif-enable").onclick = function() {
-    if ('serviceWorker' in navigator && 'PushManager' in window) {
-        console.log('Service Worker and Push is supported');
-
-        navigator.serviceWorker.register('sw.js')
-        .then(function(swReg) {
-            console.log('Service Worker is registered', swReg);
-            swRegistration = swReg;
-        })
-        .catch(handleSetupError);
-    } else {
-        handleSetupError("Push messaging is not supported by your device.")
-    }
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+    navigator.serviceWorker.register('boopservice.js')
+    .then(function(swReg) {
+        console.log('Service Worker is registered', swReg);
+        swRegistration = swReg;
+        initializeUI()
+    }).catch(handleSetupError);
+} else {
+    handleSetupError("Push messaging is not supported by your device.")
+    $("notif-enable").onclick = null;
 }
+
+var isSubscribed = false;
+function initializeUI() {
+    swRegistration.pushManager.getSubscription()
+    .then(function(subscription) {
+        isSubscribed = !(subscription === null);
+        if (isSubscribed) {
+            console.log('User IS subscribed.');
+            $("notif-enable").className="button button-selected";
+        } else {
+            console.log('User is NOT subscribed.');
+            $("notif-enable").className="button button-active";
+        }
+
+
+        $("notif-enable").onclick = function() {
+            $("notif-enable").disabled = true;
+            $("notif-enable").className = "button button-processing";
+            if (isSubscribed) {
+                // TODO: Unsubscribe user
+            } else {
+                // subscribeUser();
+            }
+        }
+        $("notif-enable").disabled = false;
+    }).catch(handleSetupError);
+}
+

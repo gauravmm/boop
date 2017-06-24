4134e57a7df5557d45ba2a880ca3763c93ae2f7d
Gaurav Manek <gaurav@gauravmanek.com>
2017-06-24 10:15:03 +0800

    Basic setup of sw.js

--- web/main.js
+++ web/main.js
@@ -1,18 +1,31 @@
+"use strict";
 
+// This is based loosely on the tutorial at:
+// https://developers.google.com/web/fundamentals/getting-started/codelabs/push-notifications/
 
-if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
+function $(id) {
+    return document.getElementById(id);
+}
 
-  navigator.serviceWorker.register('sw.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-
-    swRegistration = swReg;
-  })
-  .catch(function(error) {
+function handleSetupError(error){
     console.error('Service Worker Error', error);
-  });
-} else {
-  console.warn('Push messaging is not supported');
-  pushButton.textContent = 'Push Not Supported';
+    $("notif-enable").className = "button";
+    $("notif-enable-err").className = "error";
+    $("notif-enable-err-msg").innerHTML = error;
+}
+
+var swRegistration = null;
+$("notif-enable").onclick = function() {
+    if ('serviceWorker' in navigator && 'PushManager' in window) {
+        console.log('Service Worker and Push is supported');
+
+        navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+            console.log('Service Worker is registered', swReg);
+            swRegistration = swReg;
+        })
+        .catch(handleSetupError);
+    } else {
+        handleSetupError("Push messaging is not supported by your device.")
+    }
 }
